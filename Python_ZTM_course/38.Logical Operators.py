ca1184c81598c59df4a469360561eb8402390a25
Gokulvasan <gokulvmkg@gmail.com>
2023-06-10 06:59:00 +0530

    basic python files

--- Python_ZTM_course/38.Logical Operators.py
+++ Python_ZTM_course/38.Logical Operators.py
@@ -10,3 +10,18 @@ Logical Operators:
 """
 it allows us to perform logic between two things.
 """
+
+is_magician = False
+is_expert = True
+
+# check if magician AND expert: "you are a master magician"
+if is_magician and is_expert:
+    print("You are a master magician")
+
+# check if magician but not expert: "at least you're getting there"
+elif is_magician and not is_expert:
+    print("at least you're getting there")
+
+# if you're not a magician: 
+elif not is_magician:
+    print("You need magic powers")
