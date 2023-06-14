0d886f33fe72ddb19f9d89f5ecb4293003a90dfe
Gokulvasan <gokulvmkg@gmail.com>
2023-06-14 20:49:54 +0530

    basic python file

---
+++ Python_ZTM_course/50.Functions.py
@@ -0,0 +1,12 @@
+"""
+Python have a two type of functions
+
+1) Built-in functions.
+2) User defined functions.
+"""
+
+def hello(): # func name like a variable.
+    print("hello")
+
+print(hello) # This is just the location in memory.
+# hello() # Func calling
---
+++ Python_ZTM_course/51. Parameters and Arguments.py
@@ -0,0 +1,15 @@
+"""
+Parameters are used when we define the function and 
+Arguments are used when we call the function.
+"""
+
+# Parameters
+
+def welcome(name, emoji):
+    print(f'hellooo {name}{emoji}')
+
+# Argument
+
+welcome('Gokulvasan', '😇')
+welcome('MuthuRaj', '😉')
+welcome('Vimal', '😊')
